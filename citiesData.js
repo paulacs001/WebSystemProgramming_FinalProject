ae406fdf1f10a76c8784b952136049b5fa5ba3a9
rtyu4236 <rtyu4236@gmail.com>
2021-12-01 01:30:56 +0900

    add vue project

---
+++ citiesData.js
@@ -0,0 +1,35 @@
+//city list
+export default {
+    aCities: [{
+        name: 'Seoul',
+        img: require('@/assets/seoulCity.jpeg'),
+        desc: 'The N Seoul Tower (Korean: N서울타워), officially the YTN Seoul Tower and commonly known as Namsan Tower or Seoul Tower, is a communication and observation tower located on Namsan Mountain in central Seoul, South Korea. The 236-meter (774 ft)-tall tower marks the second highest point in Seoul.',
+        address: '105 Namsangongwon-gil, Yongsan 2(i)ga-dong, Yongsan-gu, Seoul',
+        reveal: false,
+    },
+    {
+        name: 'Busan',
+        img: require('@/assets/busan.jpeg'),
+        reveal: false,
+    },
+    {
+        name: 'Jeju Island',
+        img: require('@/assets/jeju.jpeg'),
+        reveal: false,
+    },
+    {
+        name: 'Daegu',
+        img: 'https://picsum.photos/id/1068/400/300', //this
+        reveal: false,
+    },
+    {
+        name: 'Jeongju',
+        img: 'https://picsum.photos/id/1068/400/300', //this
+        reveal: false,
+    },
+    {
+        name: 'Yeosu',
+        img: 'https://picsum.photos/id/1068/400/300', //this
+        reveal: false,
+    }]
+}
