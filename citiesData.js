de4a2b1267d2911a20c99321109592cf0553e1a5
rtyu4236 <rtyu4236@gmail.com>
2021-12-01 12:30:21 +0900

    add components in vue project

--- citiesData.js
+++ citiesData.js
@@ -2,19 +2,19 @@
 export default {
     aCities: [{
         name: 'Seoul',
-        img: require('@/assets/seoulCity.jpeg'),
+        img: require('@/assets/images/seoulCity.jpeg'),
         desc: 'The N Seoul Tower (Korean: N서울타워), officially the YTN Seoul Tower and commonly known as Namsan Tower or Seoul Tower, is a communication and observation tower located on Namsan Mountain in central Seoul, South Korea. The 236-meter (774 ft)-tall tower marks the second highest point in Seoul.',
         address: '105 Namsangongwon-gil, Yongsan 2(i)ga-dong, Yongsan-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Busan',
-        img: require('@/assets/busan.jpeg'),
+        img: require('@/assets/images/busan.jpeg'),
         reveal: false,
     },
     {
         name: 'Jeju Island',
-        img: require('@/assets/jeju.jpeg'),
+        img: require('@/assets/images/jeju.jpeg'),
         reveal: false,
     },
     {
--- tourData.js
+++ tourData.js
@@ -3,56 +3,56 @@
 export default {
     Seoul: [{
         name: 'Namsan Tower',
-        img: 'src/namsan.jpg',
+        img: '', //require('@/assets/images/namsan.jpg'),
         desc: 'The N Seoul Tower (Korean: N서울타워), officially the N Seoul Tower and commonly known as Namsan Tower or Seoul Tower, is a communication and observation tower located on Namsan Mountain in central Seoul, South Korea. The 236-meter (774 ft)-tall tower marks the second highest point in Seoul.',  
         address: '105 Namsangongwon-gil, Yongsan 2(i)ga-dong, Yongsan-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Yeouido Hangang Park',
-        img: 'src/yeouido.jpg',
+        img: '', //require('@/assets/images/yeouido.jpg'),
         desc: 'Yeouido Park offers a forest, a lawn and an ecological pond in the heart of the city. Many citizens of Seoul come to this park to enjoy light exercise or walk. You can enjoy roller blading and a bicycle ride in the Culture Center, which is located in the middle of this park.',
         address: '330, Yeouidong-ro, Yeongdeungpo-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Gyeongbokgung Palace',
-        img: 'Images/gyeongbokgung.png',
+        img: '', //require('@/assets/images/gyeongbokgung.png'),
         desc: 'It was the largest palace built during the Joseon Dynasty. When visiting Gyeongbokgung Palace, the National Palace Museum and the National Folk Museum are also good to visit together. Night opening, which enters after 7 p.m. is popular.',
         address: 'Gyeongbokgung Palace, 161 Sajik-ro, Jongno-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Cheonggyecheon',
-        img: 'Images/cheonggyecheon.jpeg',
+        img: '', //require('@/assets/images/cheonggyecheon.jpeg'),
         desc: 'At the starting point of Cheonggyecheon Stream, there is a candle fountain with three-color lights and a two-stage waterfall that falls 4m below. If you follow Cheonggyecheon Stream, you will find Dongdaemun DDP, a landmark in Seoul where rest, culture, and history coexist.',  
         address: 'Changshin-dong, Jongno-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Insadong',
-        img: 'Images/insadong.png',
+        img: '', //require('@/assets/images/insadong.png'),
         desc: 'Insadong has long been loved by artists and writers. They gathered to form a commercial district and created Insadong, which is proud to be a space for traditional culture. It is the town with the deepest knowledge of culture and arts, and there are many traditional souvenir shops.',
         address: '44 Ssamji-gil, Insadong-gil, Jongno-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Bukchon Hanok Village',
-        img: 'Images/hanokvillage.jpeg',
+        img: '', //require('@/assets/images/hanokvillage.jpeg'),
         desc: 'It is the only place in Seoul where traditional hanoks are gathered. Hanoks, historical and cultural resources, museums, and workshops are located throughout the winding maze-like alleys. The Bukchon Cultural Center is holding traditional cultural lectures such as calligraphy, tea ceremony, and pansori, as well as cultural events such as movie screenings and concerts.', 
         address: '37 Gyedong-gil, Jongno-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Seoul Forest',
-        img: 'Images/seoulforest.jpeg',
+        img: '', //require('@/assets/images/seoulforest.jpeg'),
         desc: 'Seoul Forest consists of four distinctive spaces: Culture and Arts Park, Experience Learning Center, Ecological Forest, and Wetland Ecological Center, and borders the Hangang River. You can enjoy the wonderful scenery of Eungbongsan Mountain, located opposite Seoul Forest, like a mirror on a mirror pond.',  
         address: '273 Ttukseom-ro, Seongdong-gu, Seoul',
         reveal: false,
     },
     {
         name: 'Naksan Park',
-        img: 'Images/naksan.png',
+        img: '', //require('@/assets/images/naksan.png'),
         desc: 'Naksan Park is a night view spot where you can walk along the fortress of Hanyang City Wall, take a walk, and enjoy the night view of Seoul. The road to the mountain is divided into Hyehwamun, Dongdaemun, and Daehak-ro. We recommend going up through Ewha Village, where murals are located.',
         address: '41 Naksan-gil, Jongno-gu, Seoul',
         reveal: false,
@@ -60,14 +60,14 @@ export default {
 
     Busan: [{
         name: 'Gwangalli',
-        img: 'Images/gwangalli.png',
+        img: '', //require('@/assets/images/gwangalli.png'),
         desc: '-.',
         address: '219, Gwangan Beach-ro, Suyeong-gu, Busan',
         reveal: false,
     },
     {
         name: 'Haeundae',
-        img: 'Images/haeundae.png',
+        img: '', //require('@/assets/images/haeundae.png'),
         desc: '-.',
         address: 'U-dong, Haeundae-gu, Busan',
         reveal: false,
@@ -113,6 +113,5 @@ export default {
         desc: '-.',
         address: '865-48, Choryang-dong, Dong-gu, Busan',
         reveal: false,
-    }],
-    //add here
+    }]
 }
